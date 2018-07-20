0517c19ab50a29e7c6366b0d5d92719c3565ac87
Beatrice Mendoza <mendoza@nyu.edu>
2018-07-20 01:57:41 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,7 +13,7 @@ def valid_move?(board, index)
     return false
   end
 end
- 
+
 def position_taken?(board, index)
   if board[index] == " " || board[index] == "" || board[index] == nil
     return false
@@ -28,4 +28,17 @@ end
 
 def move(board, index, character="X")
   board[index] = character
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+  else
+    until valid_move?(board, index)
+      puts "Please enter 1-9:"
+    end
+  end
 end
