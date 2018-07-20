35402dcd997a320eddd10f664f1faf9ca52c9b00
Beatrice Mendoza <mendoza@nyu.edu>
2018-07-20 02:42:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -36,7 +36,6 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
-    display_board(board)
   else
     until valid_move?(board, index)
       turn(board)
